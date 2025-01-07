afb07a25aa66877a885dc117921cdf32edcb1dda
Juan Manuel <juanmi_4000@hotmail.es>
2025-01-07 11:45:47 +0100

    Elimino imports y leo las habilidades desde un json

--- src/home/views.py
+++ src/home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.conf import settings
+import json
+import os
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    json_ruta = os.path.join(settings.BASE_DIR, 'home/static/json/habilidades.json')
+    with open(json_ruta) as habilidades_json:
+        habilidades = json.load(habilidades_json)
+    return render(request, 'index.html', {'habilidades': habilidades})
 
 def trained_ia_models(request):
     return render(request, 'trained_ia_models.html')
